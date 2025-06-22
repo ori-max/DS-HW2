27cca6e546f005a30b016addd27c0e9968fcf8cb
LAPTOP-02CUGFMQ\oribo <jon444co@gmail.com>
2025-06-22 15:11:19 +0300

    Implemented push function

--- Stack.cpp
+++ Stack.cpp
@@ -1,3 +1,5 @@
+#include <stdexcept>
+
 #include "stack.h"
 template<typename T>
 int Stack<T>::getSize() const {
@@ -7,8 +9,55 @@ template<typename T>
 void Stack<T>::setSize(int size) {
     this->size = size;
 }
+
 template<typename T>
-T* Stack<T>::getItem(int id) const {
-    int index = hash[id];
-    return data[index];
+void Stack<T>::push(T item) {
+    if (size >= capacity) {
+        //This part should move data into a new bigger array
+        int oldCapacity = capacity;
+        capacity *= 2;
+        T** temp = new T* [capacity];
+        for (int i = 0; i < size; i++) {
+            temp[i] = data[i];
+        }
+        delete [] data;
+        data = temp;
+        //This part should move hash into a new bigger array
+        IntNode** temp2 = new IntNode* [capacity];
+        for (int i = 0; i < oldCapacity; i++) {
+            if (hash[i]!=nullptr) {
+                IntNode* currIndex = hash[i];
+                while(currIndex!=nullptr) {
+                    int currId = currIndex->id;
+                    int firstIndex = currId%capacity;
+                    if (temp2[firstIndex]!=nullptr) {
+                        IntNode* currNode = temp2[firstIndex];
+                        while(currNode->next!=nullptr) {
+                            currNode = currNode->next;
+                        }
+                        currNode->next = currIndex;
+                    }
+                    else {
+                        temp2[firstIndex] = currIndex;
+                    }
+                    currIndex = currIndex->next;
+                }
+            }
+        }
+        delete [] hash;
+        hash = temp2;
+    }
+    int currId = item.getId(); //We need to make sure T types have this function!!!
+    int firstIndex = currId%capacity;
+    if (hash[firstIndex]!=nullptr) {
+        IntNode* currNode = hash[firstIndex];
+        while(currNode->next!=nullptr) {
+            currNode = currNode->next;
+        }
+        currNode->next = new IntNode(currId, size);
+    }
+    else {
+        hash[firstIndex] = new IntNode(currId, size);
+    }
+    data[size++] = item;
 }
